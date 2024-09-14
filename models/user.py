04903a31770917b53657c285750164170d6fd86d
terrymanzi <m.terry@alustudent.com>
2024-09-14 22:17:08 +0300

    update

--- models/user.py
+++ models/user.py
@@ -2,11 +2,10 @@
 """defines user class that inherits from the parent class"""
 from models.base_model import BaseModel
 
-
+"""User class inherits from BaseModel."""
 class User(BaseModel):
- """User class inherits from BaseModel, stores information about users."""
-
- email = ""
- password = ""
- first_name = ""
- last_name = ""
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
